1249418af34d7742c4776770b3fe7e989b6b6b4e
Vincent Mancuso <S01181107@acad.tri-c.edu>
2018-11-04 15:23:48 -0500

    Lab 2

---
+++ Vinny Mancuso Lab 2 Person.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Lab_2
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+        public static int Add;
+        public static double SumOfAllAges;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + LastName;
+
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(this.GetFullName());
+            System.Console.WriteLine(this.Age);
+        }
+
+        public void AskForNameAndAge()
+        {
+            System.Console.WriteLine("What is your First Name?");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is Your Last Name?");
+            this.LastName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is your Age?");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            Person.Add++;
+            Person.SumOfAllAges += this.Age;
+
+        }
+
+        public static double AverageAge()
+        {
+            return (double)Person.SumOfAllAges / (double)Person.Add;
+        }
+
+
+        public void CreateSpouseIfMarried()
+        {
+            System.Console.Write("Are you married? (y/n) : ");
+
+            if (System.Console.ReadLine().ToLower().StartsWith("y"))
+            {
+                this.Spouse = new Person();
+                this.Spouse.AskForNameAndAge();
+                this.Spouse.Spouse = this;
+            }
+
+        
+
+
+        }
+    }
+}
---
+++ Vinny Mancuso Lab 2 Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person();
+
+            p1.AskForNameAndAge();
+            p1.CreateSpouseIfMarried();
+
+            p2.AskForNameAndAge();
+            p2.CreateSpouseIfMarried();
+
+            System.Console.WriteLine("Average Age: " + Person.AverageAge());
+            System.Threading.Thread.Sleep(5000);
+
+            p1.PrintNameAndAge();
+            p1.Spouse.PrintNameAndAge();
+            p2.PrintNameAndAge();
+            p2.Spouse.PrintNameAndAge();
+            System.Threading.Thread.Sleep(10000);
+
+
+        }
+    }
+}
